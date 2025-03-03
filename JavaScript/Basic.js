a30e9523f50d6f824bcf724a5a08ae24548ccd93
HIT123444 <joshihiteshi126@gmail.com>
2025-03-03 21:40:07 +0530

    Javascript

---
+++ JavaScript/Basic.js
@@ -0,0 +1,25 @@
+console.log("Hello");
+// SUM FUNCTION
+function sum(a,b){
+    console.log(a+b)
+}
+console.log("Sum is:")
+sum(19,20)
+// SUBSTRACT FUNCTION
+function sub(c,d){
+    console.log(c-d);
+}
+console.log("Sub is:")
+sub(20,14)
+// MULTIPLY FUNCTION
+var mult=(e,f)=>{
+    console.log(e*f);
+}
+console.log("Multiply is:")
+mult(2,3)
+// DIVIDE FUNCTION
+var divide=(i,h)=>{
+    console.log(i/h);
+}
+console.log("Division is:")
+divide(20,2)
